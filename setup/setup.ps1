fd8841eaedf813696bb55e1448977f9801805d45
Lau Kar Rui <e0032073@u.nus.edu>
2018-05-20 22:29:32 +0800

    Add setup.ps1 to install dependencies

---
+++ setup/setup.ps1
@@ -0,0 +1,39 @@
+# Obtained and modified from
+# https://github.com/jayharris/dotfiles-windows/blob/master/deps.ps1
+
+# Check to see if we are currently running "as Administrator"
+if (!(Verify-Elevated)) {
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   $newProcess.Verb = "runas";
+   [System.Diagnostics.Process]::Start($newProcess);
+
+   exit
+}
+
+### Package Providers
+Write-Host "Installing Package Providers..." -ForegroundColor "Yellow"
+Get-PackageProvider NuGet -Force | Out-Null
+# Chocolatey Provider is not ready yet. Use normal Chocolatey
+#Get-PackageProvider Chocolatey -Force
+#Set-PackageSource -Name chocolatey -Trusted
+
+
+### Install PowerShell Modules
+Write-Host "Installing PowerShell Modules..." -ForegroundColor "Yellow"
+Install-Module Posh-Git -Scope CurrentUser -Force
+Install-Module PSWindowsUpdate -Scope CurrentUser -Force
+Install-Module Get-ChildItemColor -Scope CurrentUser -Force
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattparkes/PoShFuck/master/Install-TheFucker.ps1'))
+Install-Module -Name oh-my-posh -Scope CurrentUser -Force
+
+### Chocolatey
+Write-Host "Installing Desktop Utilities..." -ForegroundColor "Yellow"
+if ((which cinst) -eq $null) {
+    iex (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
+    Refresh-Environment
+    choco feature enable -n=allowGlobalConfirmation
+}
+
+Refresh-Environment
+
